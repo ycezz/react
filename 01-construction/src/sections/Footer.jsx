e80ccf9b7a76fcbd4445a5042887a17afac90894
Putri Mila <putrimiila.fx@gmail.com>
2024-10-31 20:29:50 +0800

    add

--- 01-construction/src/sections/Footer.jsx
+++ 01-construction/src/sections/Footer.jsx
@@ -1,7 +1,30 @@
 import React from "react";
+import { FaCopyright } from "react-icons/fa6";
+import { Link } from "react-scroll";
+import { FaArrowUp } from "react-icons/fa";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <>
+      <div className="bg-black text-white flex justify-center items-center gap-2 p-5">
+        {" "}
+        <FaCopyright className="fill-yellow-500 lg:size-5 size-8" />{" "}
+        <p className="text-lg text-center">
+          Copyright 2024, Prime Construction, All Rights Reserved
+        </p>
+      </div>
+
+      {/* scroll to top button */}
+      <div
+        id="icon-box"
+        className="bg-yellow-500 text-black p-3 rounded-full hover:bg-black hover:text-white cursor-pointer fixed lg:bottom-6 right-6 bottom-6"
+      >
+        <Link to="hero" spy={true} offset={-100} smooth={true}>
+          <FaArrowUp className="size-6" />
+        </Link>
+      </div>
+    </>
+  );
 };
 
 export default Footer;
--- 01-construction/src/sections/Header.jsx
+++ 01-construction/src/sections/Header.jsx
@@ -14,7 +14,7 @@ const Header = () => {
     setIsMenuOpen(false);
   };
 
-  const navItem = [
+  const navItems = [
     {
       link: "Home",
       path: "home",
@@ -38,7 +38,67 @@ const Header = () => {
   ];
 
   return (
-    <nav className="w-full flex bg-white justify-between items-center gap-1 lg:px-16 px-6 py-4 sticky top-0 z-50"></nav>
+    <nav className="w-full flex bg-white justify-between items-center gap-1 lg:px-16 px-6 py-4 sticky top-0 z-50">
+      {/* cari tahu import font daro Googlefonts */}
+      <h1 className="text-black md:text-4xl text-3xl font-bold font-rubik">
+        Prime
+        <span className="text-yellow-500 italic font-rubik"> Structure</span>
+      </h1>
+      <ul className="lg:flex justify-center items-center gap-6 hidden">
+        {navItems.map(({ link, path }) => (
+          <Link
+            key={path}
+            className="text-black uppercase font-bold cursor-pointer p-3 rounded-full hover:bg-yellow-500 hover:text-white text-[15px]"
+            to={path}
+            spy={true}
+            offset={-100}
+            smooth={true}
+          >
+            {link}
+          </Link>
+        ))}
+      </ul>
+
+      <button className="bg-yellow-500 hover:bg-black hover:text-white text-black px-10 py-3 rounded-full font-semibold transform hover:scale-105 transition-transform duration-300 cursor-pointer md:flex hidden">
+        REACH US
+      </button>
+
+      {/* mobile menu navbar */}
+      <div
+        className="flex justify-between items-center lg:hidden mt-3"
+        onClick={toggleMenu}
+      >
+        <div>
+          {isMenuOpen ? (
+            <FaXmark className="text-yellow-500 text-3xl cursor-pointer" />
+          ) : (
+            <FaBars className="text-yellow-500 text-3xl cursor-pointer" />
+          )}
+        </div>
+      </div>
+
+      <div
+        className={`${
+          isMenuOpen ? "flex" : "hidden"
+        } w-full h-fit bg-yellow-500 p-4 absolute top-[72px] left-0`}
+        onClick={closeMenu}
+      >
+        <ul className="flex flex-col justify-center items-center gap-2 w-full">
+          {navItems.map(({ link, path }) => (
+            <Link
+              key={path}
+              className="text-black uppercase font-semibold cursor-pointer p-2 rounded-lg hover:bg-black hover:text-white text-center w-full"
+              to={path}
+              spy={true}
+              offset={-100}
+              smooth={true}
+            >
+              {link}
+            </Link>
+          ))}
+        </ul>
+      </div>
+    </nav>
   );
 };
 
