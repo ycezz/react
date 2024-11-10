3f84b6aaf066839e24e68d59a0c605980bf5e39d
Putri Mila <putrimiila.fx@gmail.com>
2024-11-10 16:51:56 +0800

    add

--- 02-saas-landingpage/src/App.jsx
+++ 02-saas-landingpage/src/App.jsx
@@ -6,6 +6,7 @@ import Pricing from "./sections/Pricing";
 import Faq from "./sections/Faq";
 import Testimonials from "./sections/Testimonials";
 import Download from "./sections/Download";
+import Footer from "./sections/Footer";
 
 const App = () => {
   return (
@@ -17,6 +18,7 @@ const App = () => {
       <Faq />
       <Testimonials />
       <Download />
+      <Footer />
     </main>
   );
 };
